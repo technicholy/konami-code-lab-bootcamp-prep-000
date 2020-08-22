2a90f08dc39814b338e49a825696018d796b812a
Clayton Pierce <bamer78@gmail.com>
2020-08-22 19:35:47 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,50 @@ const codes = [
   "b",
   "a"
 ];
-
+var capList = [];
+function onKeyDownHandler(e){
+  const key = e.key;  
+  switch (key){
+    case ((key === "ArrowUp") && (capList === [])):
+      capList.push(key);
+      break;
+    case ((key === "ArrowUp") && (capList[0] === "ArrowUp")):
+      capList.push(key);
+      break;
+    case ((key === "ArrowDown") && (capList[1] === "ArrowUp")):
+      capList.push(key);
+      break;
+    case ((key === "ArrowDown") && (capList[2] === "ArrowDown")):
+      capList.push(key);
+      break;
+    case ((key === "ArrowLeft") && (capList[3] === "ArrowDown")):
+      capList.push(key);
+      break;
+    case ((key === "ArrowRight") && (capList[4] === "ArrowLeft")):
+      capList.push(key);
+      break;
+    case ((key === "ArrowLeft") && (capList[5] === "ArrowRight")):
+      capList.push(key);
+      break;
+    case ((key === "ArrowRight") && (capList[6] === "ArrowLeft")):
+      capList.push(key);
+      break;
+    case ((key === "b") && (capList[7] === "ArrowRight")):
+      capList.push(key);
+      break;
+    case ((key === "a") && (capList[8] === "b")):
+      capList.push(key);
+      break;
+    default:
+      capList = []
+    
+  }
+}
 function init() {
-  // your code here
+  document.body.addEventListener("keydown", onKeyDownHandler) {
+    if (capList === codes){
+      return capList
+    }
+    
+  }
 }
